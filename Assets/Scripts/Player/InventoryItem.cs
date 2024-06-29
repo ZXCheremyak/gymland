23bbb8152b838aab30b3b121ef453db49957cae5
kalzaker <misha311959@gmail.com>
2024-06-30 00:39:03 +0200

    Inventori

--- Assets/Scripts/Player/InventoryItem.cs
+++ Assets/Scripts/Player/InventoryItem.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public enum ItemType { Common, Rare, Epic, Legendary }
 public class InventoryItem
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public Sprite Icon { get; set; }
     public float Bonus { get; set; }
     public ItemType Rarity { get; set; }
+    public int GradeLevel { get; set; }
     public bool IsEquipped { get; set; }
 
-    public InventoryItem(string name, Sprite icon, float bonus, ItemType rarity)
+    public InventoryItem(int id, string name, Sprite icon, float bonus, ItemType rarity, int gradeLevel)
     {
+        Id = id;
         Name = name;
         Icon = icon;
         Bonus = bonus;
         Rarity = rarity;
+        GradeLevel = gradeLevel;
         IsEquipped = false;
     }
 }
